b4c1a5b11f7733c665049bd535445ace9087b21d
TabathaSlater <taslater00@gmail.com>
2022-11-17 14:36:16 -0500

    view all posts

---
+++ Tabloid/Repositories/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public interface IPostRepository
+    {
+        List<Post> GetAll();
+
+    }
+}
