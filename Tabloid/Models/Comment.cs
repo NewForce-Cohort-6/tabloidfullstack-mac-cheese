eb6f5da6a25217b25e9799c6f6cb4e00fafbb519
Jacob Jude <jacobj.jude@outlook.com>
2022-11-17 13:06:41 -0500

    stashing for PR review

--- Tabloid/Models/Comment.cs
+++ Tabloid/Models/Comment.cs
@@ -1,6 +1,19 @@
-﻿namespace Tabloid.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Tabloid.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+        
+        public int PostId { get; set; }
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public string DisplayName { get; set; }
     }
 }
