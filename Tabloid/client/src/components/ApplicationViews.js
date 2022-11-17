517454255b8b1e9cd73e89061b0c328fd1c00a97
Trey Chambers <trey.s.chambers@gmail.com>
2022-11-17 12:48:45 -0500

    View Tags working

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import CategoryForm from "./categories/CategoryForm";
 import CategoryList from "./categories/CategoryList";
 import Hello from "./Hello";
+import TagList from "./Tags/TagList";
 
 export default function ApplicationViews() {
 
@@ -11,6 +12,7 @@ export default function ApplicationViews() {
         <Route path="/" element={<Hello />} />
         <Route path="/categories" element={<CategoryList />} />
         <Route path="/createCategory" element={<CategoryForm />} />
+        <Route path="/tags" element={<TagList />} />
       </Routes>
    );
  
