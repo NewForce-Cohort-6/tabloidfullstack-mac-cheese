4ee24648710300c2c501e2a3c2eaafcf9bfcb51f
Julie Adams <julslt34@gmail.com>
2022-11-18 10:17:31 -0500

    forst friday commit

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import CategoryDelete from "./categories/CategoryDelete";
 import CategoryForm from "./categories/CategoryForm";
 import CategoryList from "./categories/CategoryList";
+import  PostList from "./posts/PostList";
+
+// import { Post } from "./posts/Post";
 import Hello from "./Hello";
 
 export default function ApplicationViews() {
@@ -12,7 +15,12 @@ export default function ApplicationViews() {
         <Route path="/" element={<Hello />} />
         <Route path="/categories" element={<CategoryList />} />
         <Route path="/createCategory" element={<CategoryForm />} />
-        <Route path="/deleteCategory/:id" element={<CategoryDelete />} />
+        <Route path="/deleteCategory/:id" element={<CategoryDelete />} />   
+        <Route path="/posts" element={<PostList />} />
+
+        {/* <Route path="/posts/" element={<PostList />} /> */}
+
+
       </Routes>
    );
  
