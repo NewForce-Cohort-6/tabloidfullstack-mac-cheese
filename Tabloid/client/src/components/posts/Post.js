e2a7d1109e7e009e85975b53b8561746fe2ca4aa
Julie Adams <julslt34@gmail.com>
2022-11-21 14:05:07 -0500

    mon 2:04

--- Tabloid/client/src/components/posts/Post.js
+++ Tabloid/client/src/components/posts/Post.js
@@ -8,6 +8,38 @@ export const Post = ({ post }) => {
   const { id } = useParams();
   const navigate = useNavigate();
 
+
+
+//   return (
+//     <Card className="m-4">
+      
+//       <CardBody>
+//         <p>
+//           <Link to={`/posts/${post.id}`}>
+//           <strong>{post.title}</strong>
+//           </Link>
+//         </p>
+//         <p>Author: {post.userProfile.displayName}</p>
+//         <p>Category: {post.category.name}</p>
+//         <p>          
+//           {
+//           post.comments.map(comment =>
+//             <div key={comment.id} >{comment.message}</div> )
+//           }
+//         </p>
+//       </CardBody>
+//     </Card>
+//   );
+// };
+
+// export default Post;
+
+
+
+
+
+
+
   return (
     // <div style={{display:'flex', letterSpacing: '.5px', alignItems: 'center', margin: '15px', flexDirection: "column", borderBottom: '1px solid gray', height: '30px', width: '500px', justifyContent: 'space-between'}}>
     //     <h5 style={{ marginRight: '15px' }}>{post.title}    {post.userProfile.firstName}   {post.userProfile.lastName}  {post.category.name}</h5>
--- Tabloid/client/src/components/posts/PostManager.js
+++ Tabloid/client/src/components/posts/PostManager.js
@@ -9,3 +9,22 @@ export const getAllPosts = () => {
 export const getPosts = (id) => {
   return fetch(`/api/post/GetWithComments/${id}`).then((res) => res.json());
 };
+
+export const getUserPosts = () => { 
+  return fetch(`/api/post/User/${currentUser.id}`)
+    .then((res) => res.json())
+};
+
+
+export const getPostById = (id) => {
+  return fetch(`/api/post/${id}`)
+      .then((res) => res.json())
+};
+
+// export const getUserPostsById = (id) => {
+//   return fetch(`/api/post/getUserPostsById?id=${id}`)
+//     .then((res) => res.json());
+// }
+
+
+
