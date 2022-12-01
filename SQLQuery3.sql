319d65d74f8651219bac5feb7a8e37668a58bb05
Julie Adams <julslt34@gmail.com>
2022-12-01 09:36:17 -0500

    first aftert thurs commit

---
+++ SQLQuery3.sql
@@ -0,0 +1,8 @@
+﻿ SELECT up.Id, up.FirstName, up.LastName, up.DisplayName, 
+                               up.Email, up.CreateDateTime, up.ImageLocation, up.UserTypeId,
+                               ut.Name AS UserTypeName
+                          FROM UserProfile up
+                               LEFT JOIN UserType ut on up.UserTypeId = ut.Id
+                         WHERE Email = 'foo@bar.com'
+
+ --select * from userprofile
